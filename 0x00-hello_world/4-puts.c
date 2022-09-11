ff3d045349f636d721c015c7c0ffa572f0116da0
beryl2 <berylakinyi150@gmail.com>
2022-09-10 20:24:29 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main -E ntry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	put("\"Programming is lke a building a multilingual puzzle");
+	return(0);
+}	
