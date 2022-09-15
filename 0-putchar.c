ca59c1144eeb86f373feb09952f31f223b7b8a94
beryl2 <berylakinyi150@gmail.com>
2022-09-15 13:42:43 -0700

    task0

---
+++ 0-putchar.c
@@ -0,0 +1,18 @@
+#include "beryl2"
+
+/**
+ * main - print beryl2, followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('y');
+	_putchar('l');
+	_putchar('2');
+
+	return (0);
+}
