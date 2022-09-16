cd171d09235ff5d5b4403e44918a2b7cd41fc38e
beryl2 <berylakinyi150@gmail.com>
2022-09-15 23:52:29 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
-#include "beryl2.h"
+#include "Horberton.h"
 /**
  * main - print Holberton, followed by new line
  * Return: 0
  */
 int main (void)
 {
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
 	_putchar('b');
 	_putchar('e');
 	_putchar('r');
-	_putchar('y');
-	_putchar('l');
-	_putchar('2');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
 	return (0);
 }
