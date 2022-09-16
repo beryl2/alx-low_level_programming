6e18f34779f4e4fc6582d492f6f3f9df321110b9
beryl2 <berylakinyi150@gmail.com>
2022-09-16 12:38:45 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,14 @@
-vi main.h
-insert mode
-#ifndef MAIN.H
-#define MAIN.H
+#include <unistd.h>
 
-int _putchar(char)
+/**
+ * _putchar - writes the character c to stdout
+ *@c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int -putchar(char c)
+{
+	return (write(1, &c,1));
+}
+
