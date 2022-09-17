d0b7f20216fc8485e474710bb3467f3ccc109072
beryl2 <berylakinyi150@gmail.com>
2022-09-17 05:01:17 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char i;
+	int i;
 	char c;
 
-	for (i=0; i<=9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
