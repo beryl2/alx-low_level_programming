247744b2af29de7d1b80048d2402f826d526c678
beryl2 <berylakinyi150@gmail.com>
2022-09-17 22:29:40 -0700

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-*_isalpha - check for lower case letter
-*@c : character to check
-*Return:0or1 
-*/
-int _isalpha(int c);
+ *  _isalpha - check for lower case letter
+ *   @c : character to check
+ *   Return:0 or 1
+ */
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
