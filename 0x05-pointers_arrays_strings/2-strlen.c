a68fd7cdcd3f88d048b2cd6fb094226bfde8bd6e
beryl2 <berylakinyi150@gmail.com>
2022-09-20 15:16:05 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - get character string
+ *
+ * @s: character to getstring
+ * Return : return length
+ * s1 is the var name for stringlenth
+ */
+int _strlen(char *s)
+{
+	int s1;
+
+	for (s1 = 0; s[s1] != '\0'; s1++)
+	{}
+	return (s1);
+}
