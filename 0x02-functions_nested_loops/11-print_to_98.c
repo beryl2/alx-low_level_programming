a9b7f75289441fbba52b0e3c62549bbe54e3de8a
beryl2 <berylakinyi150@gmail.com>
2022-12-27 20:59:11 -0800

    fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
- * @n: number to be tested 
+ * @n: number to be tested
  * Return: Always 0
  */
 
@@ -22,7 +22,7 @@ void print_to_98(int n)
 				printf(", ");
 			}
 		}
-	}else
+	} else
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
